9c6d6637a9722cd2277f56e865d59b7bbc49b927
Dave Isaacs <dave@quendi.ca>
2023-09-28 22:20:55 -0400

    POST /appointments endpoint to create an appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,5 +3,17 @@ class AppointmentsController < ApplicationController
   end
 
   def create
+    appointment = Appointment.new(appointment_params)
+    if appointment.save
+      render json: appointment, status: :created
+    else
+      render json: { errors: appointment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:starts_at, :type, :patient_name)
   end
 end
--- spec/requests/appointments_spec.rb
+++ spec/requests/appointments_spec.rb
@@ -8,10 +8,49 @@ RSpec.describe "Appointments", type: :request do
     end
   end
 
-  describe "POST /create" do
-    it "returns http success" do
-      post "/appointments"
-      expect(response).to have_http_status(:success)
+  describe "POST /appointments" do
+    context 'with valid parameters' do
+      it 'creates a new appointment' do
+        appointment_params = {
+          appointment: {
+            starts_at: '2023-09-30 10:00:00',
+            type: 'initial',
+            patient_name: 'Alice'
+          }
+        }
+
+        expect {
+          post '/appointments', params: appointment_params, as: :json
+        }.to change { Appointment.count }.by(1)
+
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+        expect(JSON.parse response.body).to include({
+          "starts_at"    => "2023-09-30T10:00:00.000Z",
+          "type"         => "initial",
+          "patient_name" => "Alice"
+        })
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'returns an unprocessable entity status' do
+        appointment_params = {
+          appointment: {
+            starts_at:    '2023-09-30 10:00', 
+            type:         'invalid-type',
+            patient_name: 'Bob'
+          }
+        }
+
+        post '/appointments', params: appointment_params, as: :json
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json; charset=utf-8') # Modify this line
+        expect(JSON.parse response.body).to eq({
+          "errors" => ["Type is unrecognized"]
+        })
+      end
     end
   end
 
